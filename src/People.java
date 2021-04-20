14d3902e5c895f7e2ee1330afe5495fc5b176d51
Pakcho523 <1820624457@qq.com>
2021-04-20 22:58:16 +0800

    Initial commit

---
+++ src/People.java
@@ -0,0 +1,13 @@
+public class People {
+    protected double weight,height;
+    public void speakHello(){
+
+    }
+    public void averageHeight(){
+
+    }
+    public void averageWeight(){
+
+    }
+}
+
